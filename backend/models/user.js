98dbc34817922e0ff1675f9632f858796d03e6e2
Emanuel Borges <emanuel.eborges@gmail.com>
2022-04-19 08:11:27 -0400

    backend_ajuste

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     
     static associate(models) {
       // define association here
-      User.hasMany(models.Device, {
+      User.belongsTo(models.Device, {
         foreignKey: 'User_fk'
       });
     }
