9c24a20cb5c38d64bdde0ce8a75eb86149e601e6
Emanuel Borges <emanuel.eborges@gmail.com>
2022-05-02 17:22:17 -0400

    frontend e backend

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -15,7 +15,6 @@ class UserController {
                 return res.status(404).json({ message: 'User not found'});
             } else {
                 const authUser = await bcrypt.compare(Password, login_one.password);
-
                 if(!authUser){
                 return res.status(422).json({
                     message: "Incorrect password",
